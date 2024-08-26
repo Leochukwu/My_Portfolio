c12b429708511420eea03f9b7aba63244e60f7ef
Leochukwu <leocchukwu@gmail.com>
2024-08-26 01:46:52 +0100

    Publications & Contact Added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   email: string = 'leocchukwu@gmail.com'
+
+
+  blogItem: any = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@chukwugodwinc').subscribe(
+      (res: any) => {
+        this.blogItem = res.items
+        console.log(this.blogItem)
+
+      },
+      (err:any) => {}
+    )
+    
+  }
 
 }
